0e2a52ef214134512cfdb2c41423bbb3ed632904
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-10 10:43:24 -0400

    Error handling

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
 import * as age from "age-encryption";
 
 async function init() {
-    await age.webauthn.createCredential({ keyName: "age encryption key 🦈" })
-    
-    const e = new age.Encrypter()
-    e.addRecipient(new age.webauthn.WebAuthnRecipient())
-    const ciphertext = await e.encrypt("Hello, age!")
-    const armored = age.armor.encode(ciphertext)
-    console.log(armored)
-    
-    const d = new age.Decrypter()
-    d.addIdentity(new age.webauthn.WebAuthnIdentity())
-    const decoded = age.armor.decode(armored)
-    const out = await d.decrypt(decoded, "text")
-    console.log(out)
+    try {
+        await age.webauthn.createCredential({ keyName: "age encryption key 🦈" })
+    } catch (error) {
+        console.error(error);
+    }
+
+    try {
+        const e = new age.Encrypter()
+        e.addRecipient(new age.webauthn.WebAuthnRecipient())
+        const ciphertext = await e.encrypt("Hello, age!")        
+        const armored = age.armor.encode(ciphertext)
+        console.log(armored)
+        
+        const d = new age.Decrypter()
+        d.addIdentity(new age.webauthn.WebAuthnIdentity())
+        const decoded = age.armor.decode(armored)
+        const out = await d.decrypt(decoded, "text")
+        console.log(out)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 init()
